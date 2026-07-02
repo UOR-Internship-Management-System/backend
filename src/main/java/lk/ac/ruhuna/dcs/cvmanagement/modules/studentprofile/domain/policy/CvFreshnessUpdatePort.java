3edda8b4c3f2b6004437ad2a3036472650357dcb
Kavindu <kavinduuni@gmail.com>
2026-07-02 14:42:58 +0530

    Refactor CvFreshnessUpdatePort and remove unused files
    - Deleted the old CvFreshnessUpdatePort interface from the application port package.
    - Moved the CvFreshnessUpdatePort interface to the domain policy package.
    - Removed the application.properties file as it was no longer needed.
    - Deleted the CvManagementApplicationTests class since it was empty and not providing any tests.

--- src/main/java/lk/ac/ruhuna/dcs/cvmanagement/modules/studentprofile/domain/policy/CvFreshnessUpdatePort.java
+++ src/main/java/lk/ac/ruhuna/dcs/cvmanagement/modules/studentprofile/domain/policy/CvFreshnessUpdatePort.java
@@ -1,4 +1,4 @@
-package lk.ac.ruhuna.dcs.cvmanagement.modules.studentprofile.application.port;
+package lk.ac.ruhuna.dcs.cvmanagement.modules.studentprofile.domain.policy;
 
 public interface CvFreshnessUpdatePort {
     // TODO: Implement in the appropriate sprint.
